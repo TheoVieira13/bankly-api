c95bf53d119d90c007f0d96feb6782f643d182c5
Theo Cardozo <theocardozo55@gmail.com>
2025-06-07 11:27:15 -0300

    Corrigindo as Classes Clinte e ClienteDTO

--- src/main/java/br/com/theo/bankly/dto/ClienteDTO.java
+++ src/main/java/br/com/theo/bankly/dto/ClienteDTO.java
@@ -1,5 +1,15 @@
 package br.com.theo.bankly.dto;
 
-public class ClienteDTO {
+import br.com.theo.bankly.model.Cliente;
 
+public record ClienteDTO (String nome,String email, String cpf, String telefone, String senha) {
+	public Cliente toCliente() {
+		Cliente cliente = new Cliente();
+		cliente.setCpf(cpf);
+		cliente.setNome(nome);
+		cliente.setEmail(email);
+		cliente.setTelefone(telefone);
+		cliente.setSenha(senha);
+		return cliente;
+	}
 }
--- src/main/java/br/com/theo/bankly/model/Cliente.java
+++ src/main/java/br/com/theo/bankly/model/Cliente.java
@@ -1,5 +1,82 @@
 package br.com.theo.bankly.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_cliente")
 public class Cliente {
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
+	private Integer idCliente;
+		
+	@Column(name = "nome_cliente", nullable = false, length = 150)
+	private String  nome;
+		
+	@Column(name = "cpf_cliente", nullable = false, unique = true, length = 11)
+	private String  cpf;
+		
+	@Column(name = "email_cliente", nullable = false, unique = true, length = 100)
+	private String  email;
+	
+	@Column(name = "telefone_cliente", nullable = false, unique = true, length = 20)
+	private String  telefone;
+		
+	@Column(name = "senha_cliente", nullable = false, length = 255)
+	private String  senha;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getTelefone() {
+			return telefone;
+		}
+
+		public void setTelefone(String telefone) {
+			this.telefone = telefone;
+		}
+
+		public String getSenha() {
+			return senha;
+		}
+
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
 
 }
