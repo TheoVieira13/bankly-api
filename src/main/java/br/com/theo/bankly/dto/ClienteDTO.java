da518f490612edabf526f5e1e162c5e9ac8906c9
Theo Cardozo <theocardozo55@gmail.com>
2025-06-09 21:44:14 -0300

    Alterando Cliente DTO

--- src/main/java/br/com/theo/bankly/dto/ClienteDTO.java
+++ src/main/java/br/com/theo/bankly/dto/ClienteDTO.java
@@ -1,8 +1,15 @@
 package br.com.theo.bankly.dto;
 
 import br.com.theo.bankly.model.Cliente;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
-public record ClienteDTO (String nome,String email, String cpf, String telefone, String senha) {
+public record ClienteDTO (@NotNull String nome,
+		                  @NotNull @Email String email,
+		                  @NotNull String cpf,
+		                  @NotNull @Min(11) String telefone,
+		                  @NotNull @Min(8) String senha) {
 	public Cliente toCliente() {
 		Cliente cliente = new Cliente();
 		cliente.setCpf(cpf);
